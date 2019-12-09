320efac4cbbe4d8fc8ed9858a1a29db69bc4794a
shimoura <renanshimoura@usp.br>
2019-12-09 19:16:24 -0300

    synaptic weight matrix added
    - the code was organized in functions (still has to be better optimized)
    - a random graph was implemented similar to Brunnel network with 80% excitatory and 20% inhibitory neurons

--- gl_cont.py
+++ gl_cont.py
@@ -1,68 +1,111 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#function
+from scipy.sparse import coo_matrix
+
+#phi function
 def phi(u, v_half, slope):
     return 1.0/(1.0+np.exp(-(u-v_half)/slope))
 
+#-----------------------------------------------------------------------------
+#function to evaluate the model
+#-----------------------------------------------------------------------------
+def evaluate(neuron_params, syn_weight, sim_params):
+    #initial conditions
+    u = np.random.normal(-58.0, 10.0, size=neuron_params['N'])
+    phi_u = np.zeros(N)          #array to store phi values
+
+    #array to store spikes
+    spk_t = []
+    spk_id = []
+
+    trun = 0.0
+    while (trun < ttotal):
+
+        #compute phi(T-dt)
+        phi_u = phi(u, v_half, slope)
+
+        S = np.sum(phi_u)
+        unif = np.random.rand()
+        dt = -np.log(unif)/S;
+
+        #compute u(T)
+        u = (u-neuron_params['u_reset'])*np.exp(-alpha*dt) + neuron_params['u_reset']
+
+        #compute phi(T)
+        phi_u = phi(u, neuron_params['v_half'], neuron_params['slope'])
+
+        unif = np.random.uniform(low=0.0, high=S)
+
+        S_new = np.sum(phi_u)
+        trun += dt
+
+        if unif <= S_new:
+            phi_cumsum = np.cumsum(phi_u)
+            neuron_id = np.where(unif<=phi_cumsum)[0][0]
+
+            u += syn_weight[neuron_id][:]
+            u[neuron_id] = neuron_params['u_reset']
+
+            spk_t.append(trun)
+            spk_id.append(neuron_id)
+
+    plt.plot(spk_t,spk_id, '.')
+    plt.show()
+
+#-----------------------------------------------------------------------------
 #parameters
-N = 200  #network size
-v_half = -45.0 #phi function parameter
-slope = 2.0 #phi function parameter
+#-----------------------------------------------------------------------------
+N = 1000       #network size
+v_half = -45.0  #phi function parameter
+slope = 2.0     #phi function parameter
 tau = 10.0
 alpha = 1/tau
 u_reset = -65.0 #reset potential
-# w = 0.15
 w = 0.15
+
+params = {'N':N, 'v_half':v_half, 'slope':slope, 'alpha':alpha, 'u_reset':u_reset}
 
 #simulation parameters
 ttotal = 1000.0
-trun = 0.0
+sim_params = {'ttotal': ttotal}
 
 np.random.seed(1000)
 
+# definition of graph
+#-----------------------------------------------------------------------------
+#all-to-all:
+#-----------------------------------------------------------------------------
 #synapses
-syn_pre = np.array([0,1])
-syn_post = np.array([1,0])
+# syn_weight = np.ones((N,N))*w
 
-#initial conditions
-u = np.zeros(N)         #initializing membrane potentials
-u = np.random.normal(-58.0, 10.0, size=N)
-phi_u = np.zeros(N)          #array to store phi values
+#-----------------------------------------------------------------------------
+#random network 80% excitatory and 20% inhibitory:
+#-----------------------------------------------------------------------------
+Nexc = int(N*0.8)
+Nin = int(N-Nexc)
 
-#array to store spikes
-spk_t = []
-spk_id = []
+p = 0.01
+Nconn_exc = int(p*Nexc*N)
+Nconn_in = int(p*Nin*N)
 
-while (trun < ttotal):
+pre_list_exc  = np.random.randint(low=0.0, high=N-1, size=Nconn_exc)
+post_list_exc  = np.random.randint(low=0.0, high=N-1, size=Nconn_exc)
+syn_weight_exc = np.ones(Nconn_exc)*w
 
-    #compute phi(T-dt)
-    phi_u = phi(u, v_half, slope)
+pre_list_in  = np.random.randint(low=0.0, high=N-1, size=Nconn_in)
+post_list_in  = np.random.randint(low=0.0, high=N-1, size=Nconn_in)
+syn_weight_in = -np.ones(Nconn_in)*w
 
-    S = np.sum(phi_u)
-    unif = np.random.rand()
-    dt = -np.log(unif)/S;
+pre_list = np.concatenate((pre_list_exc, pre_list_in))
+post_list = np.concatenate((post_list_exc, post_list_in))
+syn_weight_list = np.concatenate((syn_weight_exc, syn_weight_in))
 
-    #compute u(T)
-    u = (u-u_reset)*np.exp(-alpha*dt) + u_reset
+syn_weight = coo_matrix((syn_weight_list, (pre_list, post_list)), shape=(N, N)).toarray()
+del pre_list, pre_list_exc, pre_list_in, post_list_exc, post_list_in, \
+    syn_weight_list, syn_weight_exc, syn_weight_in
 
-    #compute phi(T)
-    phi_u = phi(u, v_half, slope)
-
-    unif = np.random.uniform(low=0.0, high=S)
-
-    S_new = np.sum(phi_u)
-    trun += dt
-
-    if unif <= S_new:
-        phi_cumsum = np.cumsum(phi_u)
-        neuron_id = np.where(unif<=phi_cumsum)[0][0]
-
-        u += w
-        u[neuron_id] = u_reset
-
-        spk_t.append(trun)
-        spk_id.append(neuron_id)
-
-plt.plot(spk_t,spk_id, '.')
-plt.show()
+#-----------------------------------------------------------------------------
+# running simulation
+#-----------------------------------------------------------------------------
+evaluate(params, syn_weight, sim_params)
